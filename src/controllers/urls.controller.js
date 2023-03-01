ab4c7a6c91c70f4b625614e4dfb967c0adaf1575
taianaazevedo <taianazev@gmail.com>
2023-03-01 15:08:26 -0300

    feat: delete url by id

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -45,7 +45,6 @@ export async function getUrlById(req, res){
 
         if(getUrl.rowCount === 0) return res.status(404).send("Essa URL não existe")
 
-
         return res.status(200).send({id: id, shortUrl: getUrl.rows[0].short_url, url: getUrl.rows[0].url })
 
     } catch (error) {
@@ -55,9 +54,50 @@ export async function getUrlById(req, res){
 }
 
 export async function getOpenUrl(req, res){
+    try {
+        
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 
 }
 
 export async function deleteUrlById(req, res){
-    
+    const { authorization } = req.headers
+    const { id } = req.params
+
+    const token = authorization?.replace("Bearer ", '')
+
+    if (!token) return res.status(401).send("Informe o token!")
+
+    try {
+        const findUser = await db.query(`
+        SELECT * FROM sessions 
+        WHERE user_token = $1
+        `, [token])
+
+        if(findUser.rowCount === 0) return res.status(401).send("Você não está logado")
+
+        const getUrl = await db.query(`
+            SELECT * FROM url WHERE id = $1
+        `, [id])
+
+        if(getUrl.rowCount === 0) return res.status(404).send("Essa URL não existe")
+
+        const findUrlByUser = await db.query(`
+            SELECT short_url FROM url 
+            WHERE user_id = $1
+        `, [findUser.rows[0].user_id])
+
+        if(findUrlByUser.rowCount === 0){
+            return res.status(401).status("Essa URL não pertence a esse usuário")
+        } else {
+            await db.query(`
+            DELETE FROM url WHERE id = $1
+            `, [id])
+        }
+        return res.status(204).send("URL excluída com sucesso")
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 }
--- src/routes/urls.route.js
+++ src/routes/urls.route.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {validateSchema} from "../middlewares/validateSchema.middleware.js"
 import { urlSchema } from "../schemas/urlsSchema.js";
-import { createShortUrl, getUrlById } from "../controllers/urls.controller.js";
+import { createShortUrl, deleteUrlById, getUrlById } from "../controllers/urls.controller.js";
 
 const routeUrl = Router();
 
 routeUrl.post("/urls/shorten", validateSchema(urlSchema), createShortUrl);
 routeUrl.get("/urls/:id", getUrlById);
 routeUrl.get("/urls/open/:shortUrl");
-routeUrl.delete("/urls/:id");
+routeUrl.delete("/urls/:id", deleteUrlById);
 
 export default routeUrl;
