ae139633a4160d7638b9da430c19944f826a1c43
taianaazevedo <taianazev@gmail.com>
2023-03-02 20:42:04 -0300

    fix: deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -9,7 +9,7 @@ const connection = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
+if (process.env.MODE === "prod") connection.ssl = true;
 
 
 export const db = new Pool(connection);
